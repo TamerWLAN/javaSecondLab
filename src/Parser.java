c9d0102e17581da781a41b4ad3c0d366132ba0f9
TamerWLAN <tamik228tamik@gmail.com>
2023-04-03 22:57:24 +0300

    Comments added, some checks added to functions checkLogical

--- src/Parser.java
+++ src/Parser.java
@@ -1,12 +1,33 @@
 
 import java.util.*;
-
+/**
+ * класс парсер, позволяющий вычислять значение арифметического выражения в ЦЕЛЫХ числах
+ * возможно использование круглых скобок, переменных и операций +-/*^
+ * вещественные значения могут принимать ТОЛЬКО переменные
+ * @author Artem Kozlitin
+ * @version 1.3
+ * @since 1.0
+ */
 public class Parser {
+    /**Словарь, отвечающий за хранение значений переменных (key - переменная, value - значение)*/
     private final Map<String,Double> variables;
+    /**Символьный массив, хранящий формулу посимвольно и без пробелов*/
     private final char[] formula;
+    /**Строковый список элементов формулы, где каждый элемент - число|операция|скобка|переменная*/
     private final List<String> elements;
+    /**Строковывй список элементов формулы, записанной в обратной польской нотации
+     * где каждый элемент - число|операция|переменная
+     */
     private final List<String> postfixElements;
+    /**Словарь, отвечающий за хранение приоритета операций при переводе в обратную польскую нотацию*/
     private final Map<String, Integer> priority;
+
+    /**
+     * Консктруктор класса, инициализирующий все необходимые струрктуры данных,
+     * заполняющий словарь приеоритетов
+     * и приводящий строковую формулу к массиву символов без пробелов
+     * @param toCalculate Исходный вид арифметического выражения, для вычисления
+     */
     public Parser(final String toCalculate) {
         if (toCalculate != null) {
             formula = toCalculate.replace(" ","").toCharArray();
@@ -26,12 +47,17 @@ public class Parser {
             throw new RuntimeException("Empty string");
         }
     }
+
+    /**
+     * Метод преобразующий выражение из массива символов
+     * в строковый список его элементов
+     */
     private void formulaToList() {
         StringBuilder number = new StringBuilder();
-        for(Character elem : formula) {
-            if(!Character.isDigit(elem)) {
+        for (Character elem : formula) {
+            if (!Character.isDigit(elem)) {
 
-                if(!number.toString().equals("")) {
+                if (!number.toString().equals("")) {
                     elements.add(number.toString());
                     number = new StringBuilder();
                 }
@@ -42,10 +68,15 @@ public class Parser {
                 number.append(elem);
             }
         }
-        if(!number.toString().equals("")) {
+        if (!number.toString().equals("")) {
             elements.add(number.toString());
         }
     }
+
+    /**
+     * Метод приводящий выражение в постфиксный вид
+     * и записывающий его в постфиксный список элементов
+     */
     private void goToReversePolandNotation() {
         Deque<String> operatorsOrder = new LinkedList<>();
 
@@ -74,6 +105,11 @@ public class Parser {
             postfixElements.add(operatorsOrder.pop());
         }
     }
+
+    /**
+     * Метод проверяющий правильность расстановки скобок в выражении на основе стека
+     * @return результат проверки: true - проблем не, false - скобки расставлены неправильно
+     */
     private boolean checkBracketsOrder() {
         Deque<Character> brackets = new LinkedList<>();
 
@@ -93,6 +129,11 @@ public class Parser {
 
         return brackets.isEmpty();
     }
+
+    /**
+     * Метод проверяющий выражение на корректность для вычисления (без учета порядка скобок)
+     * @return сообщение об ошибке, в случае проблем; пустую строку - в случае корректности
+     */
     private String checkLogical() {
         final String availableSigns = "+-*/^";
 
@@ -114,7 +155,10 @@ public class Parser {
                 return "After number, must be a sign or another number";
             }
             else if (formula[i] == '(' && !(formula[i + 1] == '(' || Character.isDigit(formula[i + 1]) || Character.isLetter(formula[i]))) {
-                return "After brackets must be a another bracket or number/variable";
+                return "After open brackets must be a another bracket or number/variable";
+            }
+            else if (formula[i] == ')' && !(formula[i + 1] == '(' || formula[i + 1] == ')' || availableSigns.indexOf(formula[i + 1]) != -1)) {
+                return "After closes brackets must be a another bracket or sign";
             }
             else if (availableSigns.indexOf(formula[i]) != -1 && availableSigns.indexOf(formula[i + 1]) != -1) {
                 return "There must not be two signs in a row";
@@ -128,6 +172,12 @@ public class Parser {
         }
         return "";
     }
+    /**
+     * Метод проверяющий общую правильность выражения
+     * вызывающий checkBracketsOrder - для скобок
+     * и checkLogical для знаков чисел и переменных
+     * в случае некорректности вызывает RuntimeException с соответствующим сообщением
+     */
     private void check() {
         if (!checkBracketsOrder()) {
             throw new RuntimeException("Bracket order error");
@@ -138,6 +188,13 @@ public class Parser {
             throw new RuntimeException(msg);
         }
     }
+
+    /**
+     * Метод для проверки , является ли данная последовательность символов числом типа double либо приводимым к нему
+     * Проверка осуществляется посредсвтом попытки парсинга строки к типу Double
+     * @param str Строка для проверки
+     * @return Результат проверки: true - число, false - не число
+     */
     private boolean isNumber(String str) {
         try {
             Double.parseDouble(str);
@@ -146,15 +203,34 @@ public class Parser {
             return false;
         }
     }
+
+    /**
+     * Метод, проверяющий является ли данная строка одним из символов допустимых операций
+     * @param str Строка для проверки
+     * @return Результат проверки: true - знак, false - не знак
+     */
     private boolean isSign(String str) {
         return (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/") || str.equals("^") );
     }
+
+    /**
+     * Метод запрашивающий у пользователя значение для текущей переменной
+     * @param variable Переменная
+     * @return Введённое пользователем значение
+     */
     private double getVarValue(String variable) {
         final Scanner sc = new Scanner(System.in);
         System.out.println(" Enter value for variable: " + variable);
 
         return sc.nextDouble();
     }
+
+    /**
+     * Метод вычисляющий значени арифметического выражения
+     * сначала вызывает метод check для проверки
+     * в случае успеха вычисляет значение с помощью двух стеков (для чисел и для знаков)
+     * @return Итоговое значение выражения
+     */
     public double compute(){
         Deque<Double> numbers = new LinkedList<>();
 
